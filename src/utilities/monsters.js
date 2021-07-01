8ae39cf3be73af838a06deb4e8f86ccad364cee7
Alex Sparrow <alexsilverhawk@gmail.com>
2021-07-01 16:53:41 -0600

    Add test monsters

---
+++ src/utilities/monsters.js
@@ -0,0 +1,22 @@
+const monsters = [
+  {
+    name: 'Death Muppet',
+    description: 'A very goofy, but deadly, foe.',
+    hitMod: 5,
+    HP: 100
+  },
+  {
+    name: 'Heck Golem',
+    description: 'A wad of animate clay that is mean',
+    hitMod: 3,
+    HP: 42
+  },
+  {
+    name: 'Bad Apple',
+    description: 'It has arms but it should not',
+    hitMod: 2,
+    HP: 15
+  }
+];
+
+export default monsters;
