0fadfd8c3edcc900970805008c72eff4ab039aec
Alex Sparrow <alexsilverhawk@gmail.com>
2021-08-10 10:29:19 -0700

    Add images for monters, and one more monster

--- src/utilities/monsters.js
+++ src/utilities/monsters.js
@@ -1,35 +1,52 @@
 const monsters = [
   {
     name: 'Death Muppet',
-    description: 'A very goofy, but deadly, foe.',
-    hitMod: 5,
+    description: 'A very goofy, but deadly, foe',
+    image: 'death_muppet.jpg',
+    strength: 5,
+    armor: 10,
     HP: 100
   },
   {
     name: 'Heck Golem',
     description: 'A wad of animate clay that is mean',
-    hitMod: 3,
+    image: 'Heck_Golem.jpg',
+    strength: 3,
+    armor: 10,
     HP: 42
   },
   {
     name: 'Bad Apple',
     description: 'It has arms but it should not',
-    hitMod: 2,
+    image: 'Bad_apple.jpg',
+    strength: 2,
+    armor: 10,
     HP: 15
   },
   {
     name: 'Straight Line',
     description: 'Bursting with loathing for bent things',
-    hitMod: 5,
+    image: 'Straight_line.jpg',
+    strength: 5,
+    armor: 10,
     HP: 12
   },
   {
     name: 'Stone Jester',
     description: 'Was only joking',
-    hotMod: 4,
+    image: 'Stone_jester.jpg',
+    strength: 4,
+    armor: 10,
     HP: 300
+  },
+  {
+    name: 'Little Vampireplane',
+    description: 'Adorable, immortal, blood-sucking conveyance',
+    image: 'little_vampireplane.jpg',
+    strength: 11,
+    armor: 15,
+    HP: 55
   }
-
 ];
 
 export default monsters;
